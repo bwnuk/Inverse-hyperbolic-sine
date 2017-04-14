ca6bfbb0c02d99de82f1bd2c22dbe28a48e7d23a
bwnuk <bartekwnuk13@gmail.com>
2017-04-14 23:50:24 +0200

    English version

--- admin/admin/admin.cpp
+++ admin/admin/admin.cpp
@@ -5,8 +5,9 @@
 #include <math.h>
 #include <Windows.h>
 #pragma warning(disable: 4996)
-FILE *plik;
+FILE *file;
 
+//						Check if it's the same element
 
 void check(double *x, double *y)
 {
@@ -20,34 +21,36 @@ void check(double *x, double *y)
 	}
 	else if ((*x) == (*y))
 	{
-		printf("Podales ten sam przedzial! ");
+		printf("You gave the same compartment! ");
 		system("pause");
 		exit(1);
 	}
 }
 
 
+//Function									Sum of series
+
 double sum(double x, double delta, double m, int* ele, int* mm)
 {
 	double suma = 0;
 	double i = 1;
-	double wyraz = x;
-	double znak = 1.0;
+	double element = x;
+	double sign = 1.0;
 	int por = *ele;
 
-	while (fabs(wyraz) > delta && por < m)
+	while (fabs(element) > delta && por < m)
 	{
 
-		suma += znak*wyraz;
-		wyraz = wyraz * (i * i* x * x) / ((i + 1)*(i + 2));
-		znak = (-1.0) * znak;
+		suma += sign * element;
+		element = element * (i * i* x * x) / ( (i + 1) * (i + 2));
+		sign = (-1.0) * sign;
 		por++;
 		i += 2;
 	}
 
-	//				Sprawdzanie czy zosta³a osi¹gnieta dok³adnoœæ
+	//				Verification Check if it has been reached precision
 
-	if (fabs(wyraz) > delta)
+	if (fabs(element) > delta)
 	{
 		*mm = 0;
 	}
@@ -64,95 +67,108 @@ double sum(double x, double delta, double m, int* ele, int* mm)
 
 int main()
 {
-	plik = fopen("wynik.txt", "w");
+	//						File to output
 
-	double a, b, dx, wynik, ww, delta;
+	file = fopen("wynik.txt", "w");
+
+	double a, b, dx, result, ww, delta;
 	int n, m;
-	int metoda = 0;
+	int method = 0;
 	int ele = 0;
 
-	printf("Podaj a i b (-1:1): \n");
+	//							Compartment
+
+	printf("Give a i b (-1:1): \n");
+
 	while (scanf("%lf %lf", &a, &b) != 2 || (a <= -1 || a >= 1 || b <= -1 || b >= 1))
 	{
-		printf("Podaj poprawne a i b (-1:1) : \n");
+		printf("Give correct a and b (-1:1) : \n");
 		while ((getchar()) != '\n');
 	}
-
 	while ((getchar()) != '\n');
 
 	check(&a, &b);
 
-	printf("Podaj ilosc przedzialow <calkowita>: ");
+
+	printf("Give amount of compartment <integer>: ");
+	
 	while (scanf("%d", &n) != 1 || n < 0)
 	{
-		printf("Nie podales ilosci przedzialow!");
+		printf("You didn't give amount of compartment!");
 		while ((getchar()) != '\n');
 	}
 	while ((getchar()) != '\n');
 
 
-	printf("Podaj dokladnosc np.(0.0001): ");
+	printf("Give precision np: (0.0001): ");
+	
 	while (scanf("%lf", &delta) != 1 || delta > 1 || delta < 0)
 	{
-		printf("Podaj poprawna dokladnosc!");
+		printf("Give correct precision!");
 		while ((getchar()) != '\n');
 	}
 	while ((getchar()) != '\n');
 
-	printf("Podaj do ktorej liczby ma liczyc: ");
+	printf("Give number of the element sum of series to counting: ");
+	
 	while (scanf("%d", &m) != 1 || m < 0)
 	{
-		printf("Podaj poprawna liczby!");
+
+		printf("Give correct number!");
 		while ((getchar()) != '\n');
+
 	}
 	while ((getchar()) != '\n');
 
-	//					Wypisywanie 
+	//					Informations 
 
 	printf("[%lf : %lf]\n", a, b);
-	printf("Dokladnosc:  %lf\n", delta);
-	printf("Ilosc przedzialow: %d\n", n);
-	printf("Ma zliczac do: %d\n", m);
+	printf("Precision:  %lf\n", delta);
+	printf("Number of compartment: %d\n", n);
+	printf("Sum to: %d\n", m);
 
 
 	dx = (b - a) / n;
 
 	int z = 1213;
-	for (double x = a; x < b; x += dx)
+	
+	
+	for (double x = a; x < b + 0.5*dx; x += dx)
 	{
+
 		ele = 0;
-		ww = sum(x, delta, m, &ele, &metoda);
-		wynik = log(x + pow(x*x + 1, 1. / 2));
+		ww = sum(x, delta, m, &ele, &method);
+		result = log(x + pow(x*x + 1, 1. / 2));
 
-		printf("\n%lf \t %12lf   \t %12lf \t %d \n", x, wynik, ww, ele);
+		//					Terminal output
 
-		// 				Sprawdzanie czy zosta³a osi¹gnieta dok³adnoœc
+		printf("\n%lf \t %12lf   \t %12lf \t %d \n", x, result, ww, ele);
+		
+		//					File output
+		
+		fprintf(file, "\n%lf \t %12lf   \t %12lf \t %d \n", x, result, ww, ele);
 
-		if (metoda == 0)
+
+		// 				Verification Check if it has been reached precision
+
+		if (method == 0)
 		{
 			printf("Zosta³a osiagnieta liczba do ktorej ma liczcy!\n\n");
+
+			fprintf(file, "Zosta³a osiagnieta liczba do ktorej ma liczcy!\n");
 		}
 		else
 		{
 			printf("Zosta³a osiagnieta dokladnosc!\n\n");
+
+			fprintf(file, "Zosta³a osiagnieta dokladnosc!\n");
 		}
 
-		fprintf(plik, "\n%lf \t %12lf   \t %12lf \t %d \n", x, wynik, ww, ele);
+		method = 0;
 
-		// 				Sprawdzanie czy zosta³a osi¹gnieta dok³adnoœc
-
-		if (metoda == 0)
-		{
-			fprintf(plik, "Zosta³a osiagnieta liczba do ktorej ma liczcy!\n");
-		}
-		else
-		{
-			fprintf(plik, "Zosta³a osiagnieta dokladnosc!\n");
-		}
-
-		metoda = 0;
 	}
-	fclose(plik);
+	
+	fclose(file);
 	system("pause");
 
 	return 0;
